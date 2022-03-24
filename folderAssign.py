e9d3a66231a592308fdc974a4282644d097a2e83
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2022-03-23 18:09:16 -0600

    inital commit

---
+++ folderAssign.py
@@ -0,0 +1,78 @@
+import os
+from random import choice
+import shutil
+
+#Empty Arrays to store
+imgs =[]
+txt =[]
+
+#Initalize Directory Names
+scrsPath = 'img' #Main Directory where images and annotations are being stored
+#scrsAnnotationPath='annotations'
+#scrsImagesPath='images' <sub directories, no longer in use>
+
+trainPath = 'training'
+imgtrainPath='imgTraining'
+traininglabelPath='labelTraining'
+
+valPath = 'validation'
+valimgPath='imgValidation'
+vallabelPath='labelValidation'
+
+
+#90% will go into training , 10% will go into validation
+train_ratio = 0.9
+valid_ratio = 0.1
+
+
+
+totalImgCount = len(os.listdir(scrsPath))/2
+
+#sorting the files into its corresponding arrays
+for (dirname, dirs, files) in os.walk(scrsPath):
+    for filename in files:
+        if filename.endswith('.txt'):
+            txt.append(filename)
+        elif filename.endswith('.png'):
+            imgs.append(filename)
+
+
+#Counter for training and validation folders
+countForTrain = int(len(imgs)*train_ratio)
+countForValid = int(len(imgs)*valid_ratio)
+
+#Training Path
+for x in range(countForTrain):
+
+    filepng = choice(imgs) # get name of random image from origin dir
+    filetxt = filepng[:-4] +'.txt' # get name of corresponding label 
+    
+                            # /img --> .png      store in    /training ---> /imgTraining              /imgtraining ----> .png
+    shutil.move(os.path.join(scrsPath, filepng), os.path.join(trainPath, imgtrainPath), os.path.join(imgtrainPath, filepng))
+    shutil.move(os.path.join(scrsPath, filetxt), os.path.join(trainPath, traininglabelPath), os.path.join(traininglabelPath, filetxt))
+
+    #remove files from folder once it is sorted
+    imgs.remove(filepng)
+    txt.remove(filetxt)
+
+
+
+#validation directory  
+for x in range(countForValid):
+
+    filepng = choice(imgs)
+    filetxt = filepng[:-4] +'.txt' 
+
+    #move both files into train directory
+    shutil.move(os.path.join(scrsPath, filepng), os.path.join(valPath, valimgPath), os.path.join(valimgPath,filepng))
+    shutil.move(os.path.join(scrsPath, filetxt), os.path.join(valPath, vallabelPath), os.path.join(vallabelPath, filetxt))
+
+    #remove files from arrays
+    imgs.remove(filepng)
+    txt.remove(filetxt)
+
+
+
+print("Total images in folder: ", totalImgCount)
+
+ 
